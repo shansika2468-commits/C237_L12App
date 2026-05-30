1d533a828995e87107215ec8d3091a7a4d16a7fd
shansika2468-commits <shansika2468@gmail.com>
2026-05-30 17:13:41 +0800

    view reviews

--- app.js
+++ app.js
@@ -11,14 +11,51 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 // Declare any necessary variables or in-memory data structures here
+let restaurants= [
+    {id:1,
+    name:"Guzman y Gomez",
+    cuisine:"Mexican",
+    rating: 4,
+    bestFood: "Salmon Sushi",
+    review: "Affordable and fast"}
 
+    {id: 2,
+    name: "Seoul Garden",
+    cuisine: "Korean",
+    rating: 5,
+    bestFood: "Kimchi Fried Rice",
+    review: "Very tasty"}
+
+    {id:3
+    name: "Sushi Train",
+    cuisine: "Japanese",
+    rating: 3,
+    bestFood: "Salmon Sushi",
+    review: "Affordable but not the best quality"}
+    
+]
 
 // TASK: Define appropriate routes below
 // ---------------------------------------------------
 
 //Define a route to render the index page
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { restaurants });
+});
+
+//view all restaurant reviews
+app.get("/restaurants", (req, res) => {
+
+  let filteredRestaurants = restaurants;
+
+  if (req.query.cuisine) {
+
+    filteredRestaurants = restaurants.filter(r =>
+      r.cuisine === req.query.cuisine
+    );
+}
+    
+  res.render('restaurants', { restaurants: filteredRestaurants });
 });
 
 // ---------------------------------------------------
